b93ec47c1fb11f7399d4e33d597feceeb908e580
soichiro049 <44058180+soichiro049@users.noreply.github.com>
2018-12-30 04:48:42 -0800

    Add files via upload

---
+++ Assets/Scripts/KowasuButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KowasuButton : MonoBehaviour
+{
+
+    public GameObject bakuhatu;
+
+    // Use this for initialization
+    void Start()
+    {
+        FadeManager.FadeIn();//フェードイン用 Staart()内に付け足す
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ThisButton()
+    {
+
+        this.gameObject.SetActive(false);
+        Instantiate(bakuhatu, new Vector3(-1.4f, -2, -1.2f), Quaternion.identity);
+        FadeManager.FadeOut(0);//行きたいシーンの番号
+                               //FadeManager.FadeIn();
+    }
+}
---
+++ Assets/Scripts/NigeruButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NigeruButton : MonoBehaviour {
+
+    public GameObject bakuhatu;
+
+	// Use this for initialization
+	void Start () {
+        FadeManager.FadeIn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ThisButton(){
+
+        this.gameObject.SetActive(false);
+        Instantiate(bakuhatu, new Vector3(1.3f, -2, -1.2f), Quaternion.identity);
+        FadeManager.FadeOut(0);//行きたいシーンの番号
+    }
+}
